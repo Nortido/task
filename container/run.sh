f2ceb299290f05f826ad1eea38b52053b22c420e
Nortido <nortido@gmail.com>
2017-08-07 22:27:41 +0600

    Updated docker files

--- container/run.sh
+++
@@ -1,4 +0,0 @@
-chown mildberry:mildberry -R /var/www/html
-
-php-fpm --daemonize
-nginx
--- index.php
+++ index.php
@@ -2,6 +2,5 @@
 /**
  * @author Evgeny Novoselov <nortido@gmail.com>
  */
-
-ini_set('display_errors', 1);
+//ini_set('display_errors', 1);
 require_once 'app/bootstrap.php';
